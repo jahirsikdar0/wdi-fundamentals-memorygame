4e3a55e8ed23124d6a1354017d1cce41d37136f9
sikdar islam <sikdarislam@sikdar-islams-MacBook.local>
2017-05-18 17:05:42 -0400

    created game logic

---
+++ memory_game/main.js
@@ -0,0 +1,21 @@
+var cardOne = "queen";
+cardOne;
+var cardTwo = "queen";
+cardTwo;
+var cardThree = "king";
+cardThree;
+var cardFour = "king";
+cardFour;
+console.log("User flipped" + cardOne);
+console.log("User flipped" + cardThree);
+
+var cards = ["queen","queen","king","king"];
+var cardsInPlay =[0,2];
+cardsInPlay;
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped" + cardOne);
+
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped" + cardTwo);
