73006be71dbc654c5c90a9a4d91d0d488bf5af7e
sikdar islam <sikdarislam@sikdarisMacBook.fios-router.home>
2017-05-19 12:39:40 -0400

    created game logic

--- memory_game/main.js
+++ memory_game/main.js
@@ -16,7 +16,21 @@ if (cardsInPlay.length === 2) {
 	else{
 		alert("Sorry try again")
 	}
+}
+var checkForMatch = function(){
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		console.log("You found a match");
+	} else{ 
+		console.log("Sorry, try again");
+	}
+}
 
-	
+// cardId is the index of the card
+var flipCard = function(cardId) {
+	console.log("User flipped" +"cards[cardId]")
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
 
- }
+flipCard(0)
+
